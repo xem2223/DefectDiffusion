39b90f5adeb92358f26a62fe2b8bd1ba547f1230
xem2223 <112392242+xem2223@users.noreply.github.com>
2025-10-26 20:48:31 +0900

    Refactor URLs for genweb application

--- genweb/genweb/urls.py
+++ genweb/genweb/urls.py
@@ -1,11 +1,16 @@
-# synth/urls.py
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
+# genweb/urls.py
+
+from django.contrib import admin
+from django.urls import path, include  # include 함수를 추가로 import 합니다.
+from django.conf import settings        # media 파일 서빙을 위해 settings import
+from django.conf.urls.static import static # media 파일 서빙을 위해 static 함수 import
 
 urlpatterns = [
-    path('', include('synth.urls')),
+    path('admin/', admin.site.urls),
+    
+    path('', include('generator.urls')), 
 ]
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROO
