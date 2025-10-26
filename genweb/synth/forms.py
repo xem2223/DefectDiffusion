4dfb321ce45b4ac3ef04c3ae8c0a8226bb665244
xem2223 <112392242+xem2223@users.noreply.github.com>
2025-10-26 20:54:38 +0900

    Delete genweb/synth/forms.py

--- genweb/synth/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-
-class InferForm(forms.Form):
-    ok_image = forms.ImageField(label="OK 이미지")
-    cond_image = forms.ImageField(label="결함 마스크 (ControlNet 조건)")
-    mask_image = forms.ImageField(label="Inpaint 마스크", required=False)
-    prompt = forms.CharField(label="텍스트 프롬프트", widget=forms.Textarea)
